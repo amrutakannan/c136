73fad2e0dc96dbc9f296b1a6234936385b955dc4
amrutakannan <75526814+amrutakannan@users.noreply.github.com>
2023-07-01 16:25:58 +0530

    Add files via upload

---
+++ c135/static/assets/index.js
@@ -0,0 +1,63 @@
+var date = new Date()
+let display_date= "Date:" + date.toLocaleDateString()
+
+$(document).ready(function () {
+    $("#display_date").html(display_date)
+    $('#save_button').prop('disabled', true);
+})
+
+let predicted_emotion;
+$(function () {
+    $("#predict_button").click(function () {
+        let input_data = {
+            "text": $("#text").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: "/predict-emotion",
+            data: JSON.stringify(input_data),
+            dataType: "json",
+            contentType: 'application/json',
+            success: function (result) {
+                $("#prediction").html(result.data.predicted_emotion)
+                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
+                $('#prediction').css("display", "");
+                $('#emo_img_url').css("display", "");
+                predicted_emotion = result.data.predicted_emotion
+                $('#save_button').prop('disabled', false);
+            },
+            error: function (result) {
+                alert(result.responseJSON.message)
+            }
+        });
+    });
+	
+	
+    //Write the code here for AJAX call to save entry - c136
+	$("#save_button").click(function(){
+		save_data={
+			"date": display_date,
+			"text": $("#text").val(),
+			"emotion": predicted_emotion
+		}
+		console.log("Index.js")
+		$.ajax({
+			type: 'POST',
+			url: "/save-entry",
+			data: JSON.stringify(save_data),
+			dataType: "json",
+			contentType: 'application/json',
+			success: function() {
+				alert("Your entry is saved successfully")
+				window.location.reload()
+			},
+			error: function(result) {
+				alert("Error")
+			}
+		})
+		console.log("After Ajax")
+	})
+
+
+})
+
